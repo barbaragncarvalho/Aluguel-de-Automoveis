459f6a8903e0829bf7f687abf0485c600ecdea15
MTaveiras <marcosctaveira@gmail.com>
2025-03-28 16:45:36 -0300

    Inicializando Springboot

---
+++ src/main/java/com/example/aluguelautomoveis/AluguelAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package com.example.aluguelautomoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelAutomoveisApplication.class, args);
+	}
+
+}
