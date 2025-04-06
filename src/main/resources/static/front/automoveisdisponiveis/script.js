003f6c90f20d82b5ac171847044fff29a0078efe
barbaragncarvalho <barbarakawanyg@gmail.com>
2025-04-06 19:44:42 -0300

    integração de criar pedido

--- src/main/resources/static/front/automoveisdisponiveis/script.js
+++ src/main/resources/static/front/automoveisdisponiveis/script.js
@@ -35,14 +35,45 @@ function exibeAutomoveis(automoveis, idCliente) {
           <td>${automovel.modelo}</td>
           <td>${automovel.ano}</td>
           <td>${automovel.placa}</td>
-          <td><a href="../pedidosagente/pedidos.html"><button class="btnPedido">Realizar Pedido</button></a></td>
+          <td><a href="../pedidoscliente/pedidos.html"><button class="btnPedido" data-id="${automovel.id}">Realizar Pedido</button></a></td>
         `;
         tbody.appendChild(tr);
     });
 
-    /*document.querySelectorAll(".btnPedido").forEach((btn) => {
-        btn.addEventListener("click", () => {
-            window.location.href = "../pedidosagente/pedidos.html";
+    document.querySelectorAll(".btnPedido").forEach((btn) => {
+        btn.addEventListener("click", async (ev) => {
+            ev.preventDefault();
+            const automovelId = btn.getAttribute("data-id");
+            if (!idCliente) {
+                alert("Contratante não encontrado!");
+                return;
+            }
+
+            const pedido = {
+                contratante: { tipo:"contratante", id: idCliente },
+                automovel: { id: automovelId }
+            };
+
+            try {
+                const response = await fetch("http://localhost:8080/api/pedidos", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(pedido),
+                });
+
+                if (!response.ok) {
+                    alert("Erro ao criar o pedido.");
+                    return;
+                }
+
+                alert("Pedido criado com sucesso!"); 
+                window.location.href = "../listacontratantes/lista.html";
+            } catch (error) {
+                console.error("Erro ao criar pedido:", error);
+                alert("Erro na requisição para criar pedido. Tente novamente, por favor.");
+            }
         });
-    });*/
+    });
 }
